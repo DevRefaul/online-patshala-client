aff648e661b124a81ed37cd73f001f8e3f3b275d
DevRefaul <devrefaul@gmail.com>
2022-10-27 15:31:25 +0600

    conditional input on newsletter section

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useContext } from "react";
 import toast from "react-hot-toast";
+import { AuthContext } from "../../Contexts/Auth/AuthContexts";
 
 const NewsLetter = () => {
+  const authInfo = useContext(AuthContext);
+  const { user } = authInfo;
+
   const handleSubscribe = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -21,25 +26,33 @@ const NewsLetter = () => {
         </p>
       </div>
 
-      {/* input section */}
-      <form
-        onSubmit={handleSubscribe}
-        className="w-[98%] md:w-[60%] xl:w-[30%] mx-auto  relative mt-6"
-      >
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Your Email"
-          className="py-3 px-2 w-full rounded outline-none"
-        />
-        <button
-          type="submit"
-          className="absolute top-1 right-1 bg-[#DA0B4E] text-white px-4 py-2 z-10 rounded font-medium"
-        >
-          Subscribe
-        </button>
-      </form>
+      {user && user.email ? (
+        <>
+          {/* input section */}
+          <form
+            onSubmit={handleSubscribe}
+            className="w-[98%] md:w-[60%] xl:w-[30%] mx-auto  relative mt-6"
+          >
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your Email"
+              className="py-3 px-2 w-full rounded outline-none"
+            />
+            <button
+              type="submit"
+              className="absolute top-1 right-1 bg-[#DA0B4E] text-white px-4 py-2 z-10 rounded font-medium"
+            >
+              Subscribe
+            </button>
+          </form>
+        </>
+      ) : (
+        <h2 className="text-3xl font-bold text-white text-center mt-6">
+          Please Login to Subscribe
+        </h2>
+      )}
     </div>
   );
 };
