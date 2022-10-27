6cef54d896404fc91209577b4ad831966b7bfb58
DevRefaul <devrefaul@gmail.com>
2022-10-27 15:25:40 +0600

    subscribe to newsletter sections interaction done

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import toast from "react-hot-toast";
 
 const NewsLetter = () => {
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    form.reset();
+    toast.success(`Hurray ${email} have subscribed to our Newsletter`);
+  };
+
   return (
     <div className="bg-[#DA0B4E] py-20">
       <div className="w-[80%] mx-auto">
@@ -13,7 +22,10 @@ const NewsLetter = () => {
       </div>
 
       {/* input section */}
-      <form className="w-[90%] md:w-[60%] xl:w-[30%] mx-auto  relative mt-6">
+      <form
+        onSubmit={handleSubscribe}
+        className="w-[98%] md:w-[60%] xl:w-[30%] mx-auto  relative mt-6"
+      >
         <input
           type="email"
           name="email"
@@ -21,7 +33,10 @@ const NewsLetter = () => {
           placeholder="Your Email"
           className="py-3 px-2 w-full rounded outline-none"
         />
-        <button className="absolute top-1 right-1 bg-[#DA0B4E] text-white px-4 py-2 z-10 rounded font-medium">
+        <button
+          type="submit"
+          className="absolute top-1 right-1 bg-[#DA0B4E] text-white px-4 py-2 z-10 rounded font-medium"
+        >
           Subscribe
         </button>
       </form>
